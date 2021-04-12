ec3666905f56987bda0eeae73d50e7c410d1c87a
Iolanta26 <iolanta.u@gmail.com>
2021-04-12 10:22:51 +0300

    clickable circles

--- speedgame/src/App.js
+++ speedgame/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    score: 0,
     circles: [
       { id: 1, color: "green" },
       { id: 2, color: "blue" },
@@ -13,15 +14,29 @@ class App extends Component {
     ],
   };
 
+  clickHandler = (id) => {
+    console.log("wow you clicked a circle" + id);
+    this.setState({
+      score: this.state.score + 1,
+    });
+  };
+
   render() {
     const circlesList = this.state.circles.map((c) => {
-      return <Circle id={c.id} color={c.color} />;
+      return (
+        <Circle
+          id={c.id}
+          key={c.color}
+          color={c.color}
+          click={() => this.clickHandler(c.id)}
+        />
+      );
     });
 
     return (
       <div>
         <h1>SpeedGame</h1>
-        <p>Your score is: </p>
+        <p>Your score is: {this.state.score}</p>
         <div className="circles">{circlesList}</div>
         <button>Start</button>
         <button>Stop</button>
--- speedgame/src/Components/Circle.js
+++ speedgame/src/Components/Circle.js
@@ -3,10 +3,12 @@ import "./Circle.css";
 
 const Circle = (props) => {
   return (
-    <div className={`circle ${props.color}`}>
+    <div className={`circle ${props.color}`} onClick={props.click}>
       <p>{props.id}</p>
     </div>
   );
 };
+
+/* <div className="circle" style={{ backgroundColor: props.color }}> */
 
 export default Circle;

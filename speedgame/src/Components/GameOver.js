fdbf89d5eeb0369b3b57ced7fb86258e7bceba6f
Iolanta26 <iolanta.u@gmail.com>
2021-04-12 19:04:31 +0300

    endtext added

--- speedgame/src/Components/GameOver.js
+++ speedgame/src/Components/GameOver.js
@@ -5,12 +5,25 @@ const closeHandler = () => {
   window.location.reload();
 };
 
+const EndGameText = (score) => {
+  if (score < 5) {
+    return "Too bad! Try again! :)";
+  } else if (score < 10) {
+    return "Good! But you can do better!:)";
+  } else if (score < 15) {
+    return "Amazing results! Try more! :)";
+  } else if (score < 20) {
+    return "You are a true hero! :)";
+  }
+};
+
 const GameOver = (props) => {
   return (
     <div className="overlay">
       <div className="gameover_box">
         <h2>Game over</h2>
         <p>Your score is: {props.score}</p>
+        <p>{EndGameText(props.score)}</p>
         <button onClick={closeHandler}>X</button>
       </div>
     </div>

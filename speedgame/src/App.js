5e10cfb473e1a5d1d206a6e40ec253c11e709960
Iolanta26 <iolanta.u@gmail.com>
2021-04-12 10:56:17 +0300

    active circles added, start and stop buttons

--- speedgame/src/App.js
+++ speedgame/src/App.js
@@ -3,9 +3,14 @@ import Circle from "./Components/Circle";
 
 import "./App.css";
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends Component {
   state = {
     score: 0,
+    current: 0,
     circles: [
       { id: 1, color: "green" },
       { id: 2, color: "blue" },
@@ -13,12 +18,40 @@ class App extends Component {
       { id: 4, color: "purple" },
     ],
   };
+  timer = undefined;
+  pace = 1500;
 
   clickHandler = (id) => {
     console.log("wow you clicked a circle" + id);
     this.setState({
       score: this.state.score + 1,
     });
+  };
+
+  nexCircle = () => {
+    let nextActive = undefined;
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+
+    this.pace *= 0.95;
+
+    this.timer = setTimeout(this.nexCircle, this.pace);
+
+    console.log("active circle is ", this.state.current);
+  };
+
+  startHandler = () => {
+    this.nexCircle();
+  };
+
+  endHandler = () => {
+    clearTimeout(this.timer);
   };
 
   render() {
@@ -38,8 +71,8 @@ class App extends Component {
         <h1>SpeedGame</h1>
         <p>Your score is: {this.state.score}</p>
         <div className="circles">{circlesList}</div>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={this.startHandler}>Start</button>
+        <button onClick={this.endHandler}>Stop</button>
       </div>
     );
   }
